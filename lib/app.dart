76549b2fa8339a8b885e67240fd45816d2db2715
wahyualif23014 <zexvulca@gmail.com>
2025-06-12 13:02:52 +0700

    new for me dewa

--- lib/app.dart
+++ lib/app.dart
@@ -1,6 +1,8 @@
 import 'package:appwrite_flutter_starter_kit/home.dart';
 import 'package:flutter/material.dart';
 
+const String APPWRITE_PROJECT_ID = "684a698b0004fa6070e9";
+
 class AppwriteApp extends StatelessWidget {
   const AppwriteApp({super.key});
 
